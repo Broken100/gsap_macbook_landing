9ac99badad32ceaaf772e086212bd9b8154c6936
Broken100 <luxopjs@gmail.com>
2025-10-18 21:41:06 -0500

    Implement Highlights section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import { ProductViewer } from './components/ProductViewer'
-import { ScrollTrigger, SplitText } from 'gsap/all'
-import gsap from 'gsap'
-import Performance from './components/Performance'
-import Features from './components/Features'
-import Highlights from './components/Highlights'
-import Footer from './components/Footer'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
-gsap.registerPlugin(ScrollTrigger)
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+import { ProductViewer } from "./components/ProductViewer";
+import Showcase from "./components/Showcase";
 
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
@@ -18,12 +19,13 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
       <Performance />
       <Features />
       <Highlights />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
